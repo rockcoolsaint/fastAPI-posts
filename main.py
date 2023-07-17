02e8b9e4f9dba99b2b39f14b1cea547d67999492
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2023-07-17 23:25:50 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"massage": "Welcome to fastAPI"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
