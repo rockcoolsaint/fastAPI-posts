dcd4f54eccbfad4c7147ed302bcaebf47b5374c8
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2023-07-19 23:15:03 +0100

    basic endpoint paths and conventions

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
 @app.get("/")
 def root():
-    return {"massage": "Welcome to fastAPI"}
+    return {"message": "Welcome to fastAPI"}
 
 
 @app.get("/posts")
 def get_posts():
     return {"data": "This is your posts"}
+
+
+@app.post("/posts")
+def create_posts(post: Post):
+    print(post.published)
+    print(post.dict())
+    return {"data": post}
